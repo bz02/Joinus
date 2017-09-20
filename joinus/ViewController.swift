1e4d7c234d8bd0b38bd8766ab934293458cc3357
xiaobozhang <zhangxiaobo199102@gmail.com>
2017-09-19 22:58:53 -0700

    Initial Commit

---
+++ joinus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  joinus
+//
+//  Created by Xiaobo Zhang on 9/19/17.
+//  Copyright © 2017 Joinus Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
